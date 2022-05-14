d2128c3887f072fc11dc1d794c44f3261003bcf2
Your Ihor <igormusak1@gmail.com>
2022-05-14 15:35:44 +0300

    create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../style/header.scss";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <img src="img/ArrowBack.svg" alt="Back" />
+      <h1>SuperFilm</h1>
+    </div>
+  );
+};
